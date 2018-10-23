94caeefe4f8a7665debcaa05be4bc8f68156eef9
kconcon3 <hkane3sub@excite.co.jp>
2018-10-23 15:58:55 +0900

    first commit

---
+++ Assets/Scripts/Test02.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Test02 : MonoBehaviour {
+
+	void Start()
+	{
+		Sequence sequence = DOTween.Sequence();
+
+		// 1つめのシーケンス
+		sequence.Append(
+			DOTween.To(
+				() => gameObject.transform.position,	// 何に
+				(n) => transform.position = n,			// 何を
+				new Vector3(400.0f, 0.0f, 0.0f),		// どこまで(最終的な値)
+				1.0f									// 時間
+			)
+		);
+
+		// 2つめのシーケンス
+		sequence.Append(
+			DOTween.To(
+				() => gameObject.transform.position,
+				(n) => transform.position = n,
+				new Vector3(0.0f, 200.0f, 0.0f),
+				1.0f
+			)
+		);
+
+		// 3つめのシーケンス
+		sequence.AppendCallback(() => CallbackTest());	// コールバック
+
+		// 4つめのシーケンス
+		sequence.Append(
+			DOTween.To(
+				() => gameObject.transform.position,
+				(n) => transform.position = n,
+				new Vector3(0.0f, 0.0f, 0.0f),
+				2.0f
+			)
+		);
+	}
+
+	// コールバックのテスト用関数
+	void CallbackTest()
+	{
+		Debug.Log("CallbackTest");
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # UnityDOTweenTest02
+
+## 概要  
+UnityのDOTweenを試してみる Part2  
+<br />
+シーケンス再生を試してみました  
+<br /> 
+## テストした時の環境  
+Windows7 Pro 64bit  
+Unity 2018.2.10f1  
+Visual Studio Community 2017  
+DOTween v1.2.055 (Release build)  
+
+## 詳細
+
